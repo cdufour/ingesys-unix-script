26b28c5f382cf7b9e1e6991f952da22c457c5fa4
cdufour <opusidea@gmail.com>
2022-12-28 10:12:16 +0100

    comments

--- cicd/exos/exo1/src/script.py
+++ cicd/exos/exo1/src/script.py
@@ -1,5 +1,8 @@
+# import du module requests pour effectuer une requête http
 import requests
 
+# exécution d'une requête http en GET sur l'url fournie en entrée
 x = requests.get('https://w3schools.com/python/demopage.htm');
 
+# affichage du corps (format texte) de la réponse http
 print(x.text)
